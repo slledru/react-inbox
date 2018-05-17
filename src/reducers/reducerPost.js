5bf0f1b0a8d269f881ea044e8342bae219590636
slledru <slledru@msn.com>
2018-05-17 09:35:11 -0600

    cleanup unused constant

--- src/reducers/reducerPost.js
+++ src/reducers/reducerPost.js
@@ -1,11 +1,8 @@
-import { ADD_MESSAGE, OPEN_FORM } from '../constants'
+import { OPEN_FORM } from '../constants'
 
 function addMessage(state = false, action) {
   /* eslint-disable */
   switch (action.type) {
-    case ADD_MESSAGE:
-      console.log('addMessage', state, action)
-      return false
     case OPEN_FORM:
       return !state
     default:
