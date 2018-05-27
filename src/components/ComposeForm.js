c1c868251f7eb8272b16a888c47fdbf0636f8834
slledru <slledru@msn.com>
2018-05-27 11:47:12 -0600

    wrap form with Route so that after adding message it switches to the home page

--- src/components/ComposeForm.js
+++ src/components/ComposeForm.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addMessage } from '../actions/actionPost'
 import { bindActionCreators } from 'redux'
+import { Route } from 'react-router-dom'
 
 class ComposeForm extends Component {
-  onSubmit = (event) => {
+  onSubmit = (history) => (event) => {
     event.preventDefault()
     const subject = document.querySelector('#subject').value || ''
     const body = document.querySelector('#body').value || ''
@@ -12,34 +13,47 @@ class ComposeForm extends Component {
       this.props.addMessage(subject, body)
       event.target.reset()
     }
+    history.push('/')
   }
 
+/*
+<button
+type='button'
+onClick={() => { history.push('/new-location') }}
+>
+Click Me!
+</button>
+*/
   render() {
     return (
-      <form className="form-horizontal well" onSubmit={ this.onSubmit }>
-        <div className="form-group">
-          <div className="col-sm-8 col-sm-offset-2">
-            <h4>Compose Message</h4>
+      <Route render={({ history }) => (
+        <form className="form-horizontal well" onSubmit={ this.onSubmit(history) }>
+          <div className="form-group">
+            <div className="col-sm-8 col-sm-offset-2">
+              <h4>Compose Message</h4>
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label htmlFor="subject" className="col-sm-2 control-label">Subject</label>
-          <div className="col-sm-8">
-            <input type="text" className="form-control" id="subject" placeholder="Enter a subject" name="subject"/>
+          <div className="form-group">
+            <label htmlFor="subject" className="col-sm-2 control-label">Subject</label>
+            <div className="col-sm-8">
+              <input type="text" className="form-control" id="subject" placeholder="Enter a subject" name="subject"/>
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label htmlFor="body" className="col-sm-2 control-label">Body</label>
-          <div className="col-sm-8">
-            <textarea name="body" id="body" className="form-control"></textarea>
+          <div className="form-group">
+            <label htmlFor="body" className="col-sm-2 control-label">Body</label>
+            <div className="col-sm-8">
+              <textarea name="body" id="body" className="form-control"></textarea>
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <div className="col-sm-8 col-sm-offset-2">
-            <input type="submit" value="Send" className="btn btn-primary"/>
+          <div className="form-group">
+            <div className="col-sm-8 col-sm-offset-2">
+              <input type="submit" value="Send" className="btn btn-primary"/>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+        )}>
+      </Route>
+
     )
   }
 }
