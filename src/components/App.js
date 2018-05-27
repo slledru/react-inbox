4db4cc2c526eae298b74a625890c07f391c45fec
slledru <slledru@msn.com>
2018-05-27 11:02:22 -0600

    pass down toggle value for different Link

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Toolbar />
+        <Toolbar toggle={ true }/>
         <MessageList />
       </div>
     )
